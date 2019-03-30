795024c57242b32f58ded1af3b66cacd80849c60
rkpeltier <rkpeltier@gmail.com>
2019-03-29 23:51:02 -0700

    Updated friends js, home, and survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,33 +1,33 @@
 var friends = [
     {
-        name: "Claire Foster",
-        photo: "",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        name: "Clarice Foster",
+        photo: "https://wow.zamimg.com/uploads/screenshots/small/432435.jpg",
+        scores: [5, 1, 4, 4, 5]
     },
     {
         name: "Loki",
-        photo: "",
-        scores: [1, 2, 1, 1, 3, 3, 2, 1, 4, 1]
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Processed_SAM_loki.jpg/360px-Processed_SAM_loki.jpg",
+        scores: [1, 2, 1, 1, 3]
     },
     {
         name: "Mona Lisa",
-        photo: "",
-        scores: [1, 1, 4, 4, 5, 1, 3, 5, 2, 1]
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/600px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg",
+        scores: [1, 1, 4, 4, 5]
     },
     {
-        name: "Van Fanel",
-        photo: "",
-        scores: [1, 3, 2, 5, 4, 1, 2, 5, 1, 1]
+        name: "Jack Burton",
+        photo: "http://1.bp.blogspot.com/-LQ7s5KxKp2E/VYDBCP-FCUI/AAAAAAAAAOQ/V9p2jcZUrvc/s1600/jack-burton-chris-pratt-big-trouble-in-little-china.jpg",
+        scores: [1, 3, 2, 5, 4]
     },
     {
-        name: "Hitomi Kanzaki",
-        photo: "",
-        scores: [1, 2, 3, 4, 5, 1, 2, 5, 1, 1]
+        name: "David Lo Pan",
+        photo: "https://vignette.wikia.nocookie.net/villains/images/6/62/Lo-Pan.jpg/revision/latest?cb=20130911024937",
+        scores: [1, 2, 3, 4, 5]
     },
     {
-        name: "James Wong",
-        photo: "",
-        scores: [5, 2, 3, 4, 5, 1, 2, 5, 3, 1]
+        name: "Cersei Lannister",
+        photo: "https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2018%2F09%2FCersei-Lannister.jpg&w=736&h=485&c=sc",
+        scores: [5, 2, 3, 4, 5]
     }
 ];
 
