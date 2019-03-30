1b4a33fcf08b098f51e72ba0380bdbda0871eb5d
rkpeltier <rkpeltier@gmail.com>
2019-03-29 19:34:58 -0700

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Claire Foster",
+        photo: "",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Loki",
+        photo: "",
+        scores: [1, 2, 1, 1, 3, 3, 2, 1, 4, 1]
+    },
+    {
+        name: "Mona Lisa",
+        photo: "",
+        scores: [1, 1, 4, 4, 5, 1, 3, 5, 2, 1]
+    },
+    {
+        name: "Van Fanel",
+        photo: "",
+        scores: [1, 3, 2, 5, 4, 1, 2, 5, 1, 1]
+    },
+    {
+        name: "Hitomi Kanzaki",
+        photo: "",
+        scores: [1, 2, 3, 4, 5, 1, 2, 5, 1, 1]
+    },
+    {
+        name: "James Wong",
+        photo: "",
+        scores: [5, 2, 3, 4, 5, 1, 2, 5, 3, 1]
+    }
+];
+
+module.exports = friends;
